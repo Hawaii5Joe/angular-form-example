297a447dfa4272a45d085021521c5dd1616d69cf
Joseph mar <Joseph.mar@libertymutual.com>
2017-09-25 09:19:43 -0700

    form submitting data with template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  // form submit method
+  onSubmit(data: NgForm){
+    console.log(data.value)
+  }
+
+
+
+
+
+
+
+
+
 }
+
